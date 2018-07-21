f2de3439aacae17061747315f1b4ba33b32e9b29
Jack Barradell-Johns <jirbarradell-johns1@sheffield.ac.uk>
2018-07-21 15:47:55 +0100

    Added error checking for empty feed

--- skill.py
+++ skill.py
@@ -11,26 +11,52 @@ DEFAULT_GNS = 3
 DEFAULT_SIM = 3
 
 
+def generate_sim_title_response(session, count = DEFAULT_SIM):
+    session = {}
+    title = "SWC News"
+    output = "Latest sim news, starting with the newest. \n"
+
+    sim_feed = feedparser.parse(SIM_FEED)
+
+    if sim_feed.get("items"):
+        for i,item in enumerate(sim_feed["items"]):
+            if i == count:
+                break
+
+            item_title = re.sub('<[^<]+?>', '', item["title"])
+
+            output += "{}. ".format(item_title)
+            output += ". . . \n"
+
+    else:
+        output += "Sorry, no sim news available... "
+
+    reprompt = "Would you like sim, g n s, or flash news?"
+    end_session = False
+    response = generate_response(session, title, output, reprompt, end_session)
+    return response
+
+
 def generate_gns_title_response(session, count = DEFAULT_GNS):
     session = {}
     title = "SWC News"
-    output = "Latest g n s news. \n"
+    output = "Latest g n s news, starting with the newest. \n"
 
     gns_feed = feedparser.parse(GNS_FEED)
 
-    if not gns_feed.get("items"):
+    if gns_feed.get("items"):
+        for i,item in enumerate(gns_feed["items"]):
+            if i == count:
+                break
 
+            item_title = re.sub('<[^<]+?>', '', item["title"])
 
-    for i,item in enumerate(gns_feed["items"]):
-        if i == count:
-            break
+            output += "{}. ".format(item_title)
+            output += " published by {}. ".format(item["author"])
+            output += " on behalf of {}. . . \n".format(item["faction"])
 
-        title = re.sub('<[^<]+?>', '', item["title"])
-
-        output += "{}. ".format(title)
-        output += " published by {}. ".format(item["author"])
-        output += " on behalf of {}... \n".format(item["faction"])
-
+    else:
+        output += "Sorry, no g n s news available... "
 
     reprompt = "Would you like sim, g n s, or flash news?"
     end_session = False
@@ -44,17 +70,24 @@ def generate_flash_response(session, count = DEFAULT_FLASH):
 
     flash_feed = feedparser.parse(FLASH_FEED)
 
-    output = "Latest flash news. \n"
+    output = "Latest flash news, starting with the newest. \n"
 
-    for i,item in enumerate(flash_feed["items"]):
-        if i == count:
-            break
+    if flash_feed.get("items"):
+        for i,item in enumerate(flash_feed["items"]):
+            if i == count:
+                break
 
-        title = re.sub('<[^<]+?>', '', item["title"])
-        summary = re.sub('<[^<]+?>', '', item["summary"])
+            item_title = re.sub('<[^<]+?>', '', item["title"])
+            summary = re.sub('<[^<]+?>', '', item["summary"])
 
-        output += "{}. ".format(title)
-        output += "{}... \n".format(summary)
+            output += "{}. ".format(item_title)
+            if not item_title == summary:
+                output += "{}".format(summary)
+
+            output += ". . . \n"
+
+    else:
+        output += "Sorry, no flash news available..."
 
     reprompt = "Would you like sim, g n s, or flash news?"
     end_session = False
@@ -104,6 +137,11 @@ def on_intent(intent_request, session):
             return generate_gns_title_response(session, int(intent['slots']['number'].get('value')))
         else:
             return generate_gns_title_response(session)
+    elif intent_name == 'sim':
+        if intent['slots']['number'].get('value'):
+            return generate_sim_title_response(session, int(intent['slots']['number'].get('value')))
+        else:
+            return generate_sim_title_response(session) 
     else:
         raise ValueError("Invalid intent")
 
